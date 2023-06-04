57e0d4d70d3ba3eddd389c7f8ad032d26ad396e4
YIM2UL2ET <132066506+YIM2UL2ET@users.noreply.github.com>
2023-06-04 17:52:00 +0900

    Create def drawBackCardBorder(파일).py

---
+++ 프로그래밍 기초 과제 관련/코드 파일들/def drawBackCardBorder(파일).py
@@ -0,0 +1,35 @@
+import turtle as t
+
+# 펜 설정
+t.speed(0)
+t.pendown()
+t.hideturtle()
+
+# 사용 함수 설정 (1개)
+
+def drawBorder(color,size):
+    t.pencolor(color)
+    t.pensize(size)
+    for i in range(4):
+        t.circle(10,90)
+        if i % 2 == 0: t.fd(402)
+        else: t.fd(250)
+
+#------------------------------
+
+def drawBackCardBorder():
+
+    # 그릴 위치로 이동
+    t.penup()
+    t.fd(125)
+    t.rt(90)
+    t.fd(200)
+    t.lt(90)
+    t.pendown()
+
+    # 테두리 그리기
+    drawBorder("white",10)
+    drawBorder("grey",1)
+
+# 테스트
+drawBackCardBorder()
