1cbdcbb377724a04cb9f33b1e30f1dda5ed3a737
YIM2UL2ET <132066506+YIM2UL2ET@users.noreply.github.com>
2023-06-06 14:17:19 +0900

    Create 3-1 def drawCardBackground(파일).py

---
+++ 프로그래밍 기초 과제 관련/코드 파일들/3-1 def drawCardBackground(파일).py
@@ -0,0 +1,28 @@
+import turtle as t
+
+def drawCardBorder(t,color,size):
+    t.pencolor(color)
+    t.pensize(size)
+    for i in range(4):
+        t.circle(10,90)
+        if i % 2 == 0: t.fd(402)
+        else: t.fd(250)
+
+def moveto_drawBackCardBorder(t):
+    t.penup()
+    t.fd(125)
+    t.rt(90)
+    t.fd(200)
+    t.lt(90)
+    t.pendown()
+
+#----------------------------
+def drawCardBackground(t):
+    moveto_drawBackCardBorder(t)
+    t.fillcolor("#ffffff")
+    t.begin_fill()
+    drawCardBorder(t,"#ffffff",10)
+    t.end_fill()
+
+#테스트
+drawCardBackground(t)
